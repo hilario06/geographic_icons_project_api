87e5d135ae194449dcf334306fa9c6693bc2eb50
luis <hilario.huaman.6@gmail.com>
2022-07-13 12:56:20 -0500

    Passing Login Tests

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,14 +13,24 @@ module Api
       end
 
       def login
+        @user = User.find_by(email: login_params[:email])
+        # authenticate lo proporciona bcrypt
+        if @user.present? && @user.authenticate(login_params[:password]) # second condicional si el usuario hace match con el usuario que envio
+          render :show
+        else
+          render( json: { errors: I18n.t('user.bad_credentials')}, status: :bad_request)
+        end
       end
 
       private
 
+      def login_params
+        params.require(:user).permit(:email, :password)
+      end
+
       def user_params
         params.require(:user).permit(:email, :age, :password)
       end
-
     end
   end
 end
